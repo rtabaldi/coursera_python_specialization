f43f2c846bd15a43df0ada1f07ef25e642dc8392
scan-the-automator <rtabaldi@gmail.com>
2016-04-16 17:51:57 -0700

    excersize 3.3

---
+++ 3_3.py
@@ -0,0 +1,22 @@
+score = raw_input("Enter your test score: ")
+
+try:
+	float_score = float(score)
+except:
+	float_score = -1
+
+if float_score > 0:
+	if float_score < .6:
+		print "F"
+	elif float_score < .7:
+		print "D"
+	elif float_score < .8:
+		print "C"
+	elif float_score < .9:
+		print "B"
+	elif float_score <= 1:
+		print "A"
+	else:
+		print "This number is out of range, please try again"
+else:
+	print "This is not a number"
