cb79e14ee1d185224ea4746c76c51c4e8393145e
scan-the-automator <rtabaldi@gmail.com>
2016-04-16 17:14:19 -0700

    updated version

--- 3_1.py
+++ 3_1.py
@@ -1,4 +1,4 @@
-hours = raw_input("How many hours did you work?:")
+hours = raw_input("How many hours did you work?: ")
 float_hours = float(hours)
 
 pay_rate = 10.5
@@ -6,7 +6,7 @@ pay_rate = 10.5
 overtime_rate = 1.5 * pay_rate
 
 if float_hours > 40:
-	ot = hours - 40
-	print (pay_rate * 40) + (ot * overtime_rate)
+	ot = float_hours - 40
+	print "You made",(pay_rate * 40) + (ot * overtime_rate)
 else:
-	print pay_rate * float_hours
+	print "You made",pay_rate * float_hours
