bb2d56ae6dd2abf6b771ebb8481ebb792f9061bc
scan-the-automator <rtabaldi@gmail.com>
2016-04-29 23:11:47 -0700

    Exercise 8.4

---
+++ 8_4.py
@@ -0,0 +1,15 @@
+file_name = raw_input('what is your file name? ')
+
+file_handler = open(file_name)
+
+lst = []
+
+for line in file_handler:
+	words = line.split()
+
+	for word in words:
+		if word in lst: continue
+		lst.append(word)
+
+lst.sort()
+print lst
