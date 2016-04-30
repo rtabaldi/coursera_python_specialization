0e4ebce62a7fa85cc13705f1979812172d6e8499
scan-the-automator <rtabaldi@gmail.com>
2016-04-29 21:02:39 -0700

    Exercise 7.2

---
+++ 7_2.py
@@ -0,0 +1,15 @@
+file_name = raw_input('what is your files name? ')
+
+file_handler = open(file_name)
+
+count = 0
+total = 0
+
+for word in file_handler:
+	word = word.rstrip()
+	if word.startswith('X-DSPAM-Confidence:'):
+		count = count + 1
+		word = float(word[word.find('0'):len(word)])
+		total = total + word
+
+print 'Average Spam Confidence',(total/count)
