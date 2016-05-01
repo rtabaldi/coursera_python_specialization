b52335eaf19935aa269ea9c8919e089aa191ee1d
scan-the-automator <rtabaldi@gmail.com>
2016-04-30 17:49:49 -0700

    Exercise 8.5

---
+++ 8_5.py
@@ -0,0 +1,14 @@
+file_name = raw_input('what is the file name? ')
+
+file_handler = open(file_name,'r')
+
+count = []
+
+for line in file_handler:
+	if not line.startswith('From '): continue
+	words = line.split()
+	count.append(words)
+	print words[1]
+
+print "There were",len(count),"lines in the file with From"\
+" as the first word"
