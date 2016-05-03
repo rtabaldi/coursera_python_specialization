d7f9a2f9491d2789f7f8a94f21f74e8c0cab930d
scan-the-automator <rtabaldi@gmail.com>
2016-05-02 22:44:47 -0700

    Exercise 9.5

---
+++ 9_4.py
@@ -0,0 +1,19 @@
+file_name = raw_input('What is the name of your file? ')
+file_handler = open(file_name,'r')
+
+counts= {}
+bigword = None
+bigcount = None
+
+for line in file_handler:
+    if not line.startswith('From '): continue
+    words = line.split()
+    words = words[1]
+    counts[words] = counts.get(words,0) + 1
+
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+print bigword, bigcount
