b83606a89445146952bf2b7e7dab6ca76124eecb
scan-the-automator <rtabaldi@gmail.com>
2016-05-18 21:16:29 -0700

    Exercise 12.4

---
+++ 12_4.py
@@ -0,0 +1,16 @@
+import urllib
+from BeautifulSoup import *
+
+
+url = raw_input('Enter url - ')
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
+tags = soup('span')
+
+total = 0
+
+for tag in tags:
+	content = tag.contents[0]
+	total = total + int(content)
+
+print total
